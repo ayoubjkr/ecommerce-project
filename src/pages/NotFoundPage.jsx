263955c2c8fa93be2ec42e47c2a1a3485e096a24
Ayoub jkr <jakouriayoub08@gmail.com>
2025-10-06 18:44:52 +0100

    create a not found page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import { Header } from "../components/Header";
+
+export function NotFoundPage(){
+    return(
+        <>
+        <title>Not Found Page</title>
+        <Header />
+
+        
+        <div>
+          <h1> Page Not Found </h1>
+        </div>
+        
+        </>
+    )
+}
