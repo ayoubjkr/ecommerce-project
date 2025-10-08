644923729aab65df1e56678982e0cf20938d1a6b
Ayoub jkr <jakouriayoub08@gmail.com>
2025-10-08 16:51:29 +0100

    left cart up and share it between pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,19 +5,16 @@ import { Header } from '../components/Header'
 import './HomePage.css'
 import CheckMark from './../assets/images/icons/checkmark.png'
 
-export function HomePage() {
+export function HomePage({ cart }) {
 
     const [products, setProducts]= useState([])
-    const [cart, setCart]= useState([])
 
     useEffect(()=>{
         
         axios.get('/api/products').then((response)=>{
             setProducts(response.data)
         })
-        axios.get('/api/cart-items').then((response)=>{
-            setCart(response.data)
-        })
+
     
     },[])
 
